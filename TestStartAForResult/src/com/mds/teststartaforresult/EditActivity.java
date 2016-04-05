69e3f4cc81ec4ff2abde0faaa2d08657586bd9ba
KangTaehui <kang@abc.com>
2016-04-05 10:34:07 +0900

    Class Add

--- TestStartAForResult/src/com/mds/teststartaforresult/EditActivity.java
+++ TestStartAForResult/src/com/mds/teststartaforresult/EditActivity.java
@@ -1,7 +1,16 @@
 package com.mds.teststartaforresult;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class EditActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_layout);
+	}
+	
 }
+
