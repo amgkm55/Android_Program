106e26f5f44017f7831b0a58ad0686e2571288e9
KangTaehui <kang@abc.com>
2016-04-05 17:08:36 +0900

    BR 서비스의 동적등록

--- TestBR/src/com/mds/testbr/MainActivity.java
+++ TestBR/src/com/mds/testbr/MainActivity.java
@@ -1,34 +1,38 @@
 package com.mds.testbr;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	MyReceiver receiver = new MyReceiver();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
+	//BR활성화
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+		registerReceiver(receiver, filter);
 	}
-
+	//BR비활성화
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		unregisterReceiver(receiver);
 	}
+	
+	
+	
 }
