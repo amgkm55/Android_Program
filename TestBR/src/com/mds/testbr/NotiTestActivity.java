a372dc9000bba8f09dd894333b4cb290d47da05a
KangTaehui <kang@abc.com>
2016-04-05 17:40:44 +0900

    Noti생성한 곳에 Intent를 연결해서 노티를 클릭하면 사이트로 연결함

--- TestBR/src/com/mds/testbr/NotiTestActivity.java
+++ TestBR/src/com/mds/testbr/NotiTestActivity.java
@@ -3,6 +3,9 @@ package com.mds.testbr;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +29,8 @@ public class NotiTestActivity extends Activity {
 	 * 	4. Manager 클래스를 이용하여 Notification 발생하기
 	 * */ 
 	public void notiTest(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kma.go.kr"));
+		PendingIntent pIn = PendingIntent.getActivity(this, 1, i, PendingIntent.FLAG_CANCEL_CURRENT);
 		
 		
 		Notification.Builder notiB = new Notification.Builder(this);
@@ -34,7 +39,9 @@ public class NotiTestActivity extends Activity {
 		notiB.setContentText("Notification Test working..");
 		notiB.setUsesChronometer(true);
 		notiB.setContentInfo("Under Right");
+		notiB.setContentIntent(pIn);
 		notiB.setDefaults(Notification.DEFAULT_ALL);
+		
 		notiMgr.notify(1, notiB.build());
 	}
 }
